b0c116ec2f51509f4f1e511d4e06fcb3d0166f04
Pu Pu <pupu@deepglint.com>
2026-04-23 16:13:47 +0800

    chore(v2): 移除未使用的 expireTime 函数

--- v2/utils.go
+++ v2/utils.go
@@ -11,11 +11,3 @@ var now = func() int64 {
 func isExpired(expireAt, nowNano int64) bool {
 	return expireAt > 0 && nowNano > expireAt
 }
-
-// expireTime 计算过期时间戳
-func expireTime(ttl time.Duration) int64 {
-	if ttl <= 0 {
-		return 0 // 永不过期
-	}
-	return now() + int64(ttl)
-}
